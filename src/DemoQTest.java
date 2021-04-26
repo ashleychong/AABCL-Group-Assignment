96144a3e76335de41fbf24a05efd5ae2d947ec2d
ashleychong <ashleychong0820@gmail.com>
2021-04-26 14:54:13 +0800

    Simulate operation

--- src/DemoQTest.java
+++ src/DemoQTest.java
@@ -2,7 +2,7 @@ import java.util.Random;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.DelayQueue;
 
-public class DelayQDemo {
+public class DemoQTest {
     public static void main(String[] args) {
         BlockingQueue<Visitor> delayQ = new DelayQueue<>();
         Random r = new Random();
@@ -19,7 +19,7 @@ public class DelayQDemo {
                 try {
 //                    long delay = (long) (r.nextInt(2001) + 1000);
                     long delay = (r.nextInt(101) + 50) * 1000;
-                    delayQ.put(new Visitor("Element" + i, delay));
+//                    delayQ.put(new Visitor("Element" + i, delay));
 
                     long nextWindow = (r.nextInt(6) + 5) * 1000;
 //                    Thread.sleep(50);
@@ -40,7 +40,6 @@ public class DelayQDemo {
                     System.out.println(" Consumer got - " + delayQ.take().toString());
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
-                    // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
             }
